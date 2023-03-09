6a7775ca4deb84456087f51588f4b244b131395b
Frainer Encarnación <89224196+fraineralex@users.noreply.github.com>
2023-03-09 09:07:30 -0400

    NOT-3 -- BACKEND - exporta el servidor y la app
    Se exporta el servidor y la app para poder ser usados en cualquier parte del proyecto, por ejemplo en las pruebas unitarias.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import app from "./src/index.js";
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = { app, server }
